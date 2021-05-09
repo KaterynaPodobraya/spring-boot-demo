036c4fd9d145120d479ca6c9575033ee4faa9e7e
kateryna <kateryna.podobraya@gmail.com>
2021-05-09 20:44:11 +0300

    Enable tests

--- src/test/java/com/example/springbootdemo/e2eMainTest.java
+++ src/test/java/com/example/springbootdemo/e2eMainTest.java
@@ -1,31 +1,32 @@
-//package com.example.springbootdemo;
-//
-//import lombok.SneakyThrows;
-//import org.hamcrest.MatcherAssert;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.net.URI;
-//
-//import static io.restassured.RestAssured.when;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//// runs on started application
-//
-//public class e2eMainTest {
-//
-//    @Disabled
-//    @SneakyThrows
-//    @Test
-//    public void shouldReturnToy(){
-//        String body =
-//                when().get( new URI("/getToy"))
-//        .then().extract().body().asString();
-//
-//        assertThat(body).contains("Bobik");
-//
-//    }
-//
-//}
+package com.example.springbootdemo;
+
+import io.restassured.RestAssured;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class e2eMainTest {
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void shouldReturnToy() throws URISyntaxException {
+        String body = given().port(port)
+                             .when().get( new URI("/getToy"))
+                             .then().extract().body().asString();
+
+        assertThat(body).contains("Bobik");
+
+    }
+
+}
